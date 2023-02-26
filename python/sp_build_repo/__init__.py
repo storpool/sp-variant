5b7a8ab87332f34ab3c57b013f1124ed11c9257a
Peter Pentchev <pp@storpool.com>
2023-02-26 14:17:43 +0200

    python: add docstrings to some `__init__.py` files
    Change-Id: I590f62761c86fa5a6ba346be9641cbdf2552f0a8

---
+++ python/sp_build_repo/__init__.py
@@ -0,0 +1,6 @@
+"""Build the ``add-storpool-repo`` archive for all variants.
+
+Build an archive containing the shell program that can detect the current
+build variant, along with the Apt and DNF/Yum repository definitions for
+all supported variants.
+"""
---
+++ python/sp_variant/__init__.py
@@ -0,0 +1,7 @@
+"""Detect the Linux distribution and version for the StorPool build system.
+
+The ``sp-variant`` library is mainly useful within the StorPool internal
+build and QA environment, as well as the first step of installations on
+end-user systems. It examines several files and tries to determine what
+distribution and what version it is running on.
+"""
