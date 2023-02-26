b21727d530eaae664fbc63f746900077e5fa4bd5
Peter Pentchev <pp@storpool.com>
2023-02-26 13:29:24 +0200

    Add the year 2023 to the StorPool copyright notice
    Change-Id: Ibc4e43a80f0da632865ae917f05036a98e4890d4

--- python/unit_tests/test_subst.py
+++ python/unit_tests/test_subst.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021  StorPool <support@storpool.com>
+# Copyright (c) 2021 - 2023  StorPool <support@storpool.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
--- rust/yai.rs
+++ rust/yai.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2022  StorPool <support@storpool.com>
+ * Copyright (c) 2021 - 2023  StorPool <support@storpool.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
