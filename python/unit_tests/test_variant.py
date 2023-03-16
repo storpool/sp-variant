aaee6652d3809ecfa3d4306b88b4c8fc7ceb5591
Peter Pentchev <pp@storpool.com>
2023-03-16 10:06:34 +0200

    python: test the _diag_to_stderr implementation
    Add unit tests for the current state of the defs.Config class's
    handling of stdout/stderr output. It will mostly go away soon though.
    
    Change-Id: I974952d7bc7accfcd314b7beb7a67ce4853536a7

--- python/unit_tests/test_variant.py
+++ python/unit_tests/test_variant.py
@@ -2,15 +2,25 @@
 # SPDX-License-Identifier: BSD-2-Clause
 """Test the functions in the sp.variant module."""
 
-import pathlib
+from __future__ import annotations
 
-from typing import Final
+import dataclasses
+import pathlib
+import sys
+
+from unittest import mock
+
+from typing import Final, IO
 
 import pytest
 
 from sp_variant import defs
 from sp_variant import variant
 from sp_variant import vbuild
+
+
+_MSG_NOT_SEEN = "This should not be seen"
+_MSG_SEEN = "This should be seen"
 
 
 def test_get() -> None:
@@ -76,3 +86,65 @@ def test_list_all() -> None:
     pkgs_all: Final = variant.list_all_packages(var, patterns=["*"])
     print(f"{len(pkgs_all)} total packages on the system")
     assert pkgs_all
+
+
+def test_config_diag() -> None:
+    """Test the `cfg_diag`-like functionality of the `Config` class."""
+    output: list[tuple[str, IO[str]]] = []
+
+    def check(*, seen: bool, to_stderr: bool = True) -> None:
+        """Make sure the output is exactly as expected."""
+        if not seen:
+            assert not output
+            return
+
+        assert output == [(_MSG_SEEN, sys.stderr if to_stderr else sys.stdout)]
+        output.clear()
+
+    def init_cfg(
+        *, verbose: bool, diag_to_stderr: bool = True, use_setattr: bool = False
+    ) -> defs.Config:
+        """Initialize a defs.Config object in the specified way."""
+        # pylint: disable=protected-access
+        cfg = defs.Config(verbose=verbose)
+        assert cfg._diag_to_stderr  # noqa: SLF001
+        if diag_to_stderr:
+            return cfg
+
+        if use_setattr:
+            object.__setattr__(cfg, "_diag_to_stderr", False)  # noqa: FBT003
+        else:
+            cfg._diag_to_stderr = False  # type: ignore[misc]  # noqa: SLF001
+        assert not cfg._diag_to_stderr  # noqa: SLF001
+        return cfg
+
+    def mock_print(msg: str, *, file: IO[str]) -> None:
+        """Mock a print() invocation."""
+        output.append((msg, file))
+
+    check(seen=False)
+
+    cfg = init_cfg(verbose=False)
+    with mock.patch("builtins.print", new=mock_print):
+        cfg.diag(_MSG_NOT_SEEN)
+
+    check(seen=False)
+
+    cfg = init_cfg(verbose=True)
+    with mock.patch("builtins.print", new=mock_print):
+        cfg.diag(_MSG_SEEN)
+
+    check(seen=True)
+
+    # We can't set _diag_to_stderr directly, right?
+    with pytest.raises(dataclasses.FrozenInstanceError):
+        cfg = init_cfg(verbose=True, diag_to_stderr=False)
+
+    check(seen=False)
+
+    # OK, can we do stdout now?
+    cfg = init_cfg(verbose=True, diag_to_stderr=False, use_setattr=True)
+    with mock.patch("builtins.print", new=mock_print):
+        cfg.diag(_MSG_SEEN)
+
+    check(seen=True, to_stderr=False)
