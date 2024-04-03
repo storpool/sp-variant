e40e523c533884792a1c17173ad59cbf7484c022
Peter Pentchev <pp@storpool.com>
2024-04-03 12:16:01 +0300

    Drop support for Ubuntu 23.04
    It was never meant to be supported long-time; StorPool only
    really wants LTS releases, Lunar Lobster was there for
    forward-compatibility testing only.
    
    Change-Id: I8a96be7f834fef72aa14746147a2780ae7a8f3a7

--- python/sp_variant/vbuild.py
+++ python/sp_variant/vbuild.py
@@ -252,34 +252,9 @@ _VARIANT_DEF: Final[list[defs.Variant | defs.VariantUpdate]] = [
         },
     ),
     defs.VariantUpdate(
-        name="UBUNTU2304",
-        descr="Ubuntu 23.04 LTS (Lunar Lobster)",
-        parent="UBUNTU2404",
-        detect=defs.Detect(
-            filename="/etc/os-release",
-            regex=re.compile(
-                r"^ PRETTY_NAME= .* Ubuntu \s+ 23 \. 04 ",
-                re.X,
-            ),
-            os_id="ubuntu",
-            os_version_regex=re.compile(r"^23\.04$"),
-        ),
-        updates={
-            "repo": {
-                "vendor": "ubuntu",
-                "codename": "lunar",
-            },
-            "builder": {
-                "alias": "ubuntu-23.04",
-                "base_image": "ubuntu:lunar",
-                "branch": "ubuntu/lunar",
-            },
-        },
-    ),
-    defs.VariantUpdate(
         name="UBUNTU2204",
         descr="Ubuntu 22.04 LTS (Jammy Jellyfish)",
-        parent="UBUNTU2304",
+        parent="UBUNTU2404",
         detect=defs.Detect(
             filename="/etc/os-release",
             regex=re.compile(
